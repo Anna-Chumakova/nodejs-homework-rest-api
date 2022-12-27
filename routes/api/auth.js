4454e80f492e2e6ea6027e116a8ccbc83fb76f8e
Anna-Chumakova <ann020187@gmail.com>
2022-12-27 17:08:02 +0200

    ctrl register

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const ctrl = require("../../controllers/auth");
+const { ctrlWrapper } = require("../../helpers");
+const { schemasUser } = require("../../models");
+const { validateBody } = require("../../middlewares");
 
 const router = express.Router();
 
-
+router.post("./register", validateBody(schemasUser.registerSchema), ctrlWrapper(ctrl.register))
 
 module.exports = router;
