ff84444103ede34a92160457aec0f9fcd1a0e27d
Anna-Chumakova <ann020187@gmail.com>
2022-12-29 17:04:33 +0200

    token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,8 +4,10 @@ const { ctrlWrapper } = require("../../helpers");
 const { schemasUser } = require("../../models");
 const { validateBody } = require("../../middlewares");
 
+
 const router = express.Router();
 
 router.post("/register", validateBody(schemasUser.registerSchema), ctrlWrapper(ctrl.register))
+router.post("/login", validateBody(schemasUser.loginSchema), ctrlWrapper(ctrl.login))
 
 module.exports = router;
