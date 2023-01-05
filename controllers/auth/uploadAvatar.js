72a1b3481093be022d2ae1796009e57000bec742
Anna-Chumakova <ann020187@gmail.com>
2023-01-05 20:15:53 +0200

    task without jimp

--- controllers/auth/uploadAvatar.js
+++ controllers/auth/uploadAvatar.js
@@ -1,19 +1,24 @@
 const { User } = require("../../models");
 const fs = require("fs/promises");
 const path = require("path");
+//const Jimp = require("jimp");
 
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars")
 
 const uploadAvatar = async (req, res) => {
     const { _id } = req.user;
-    const { path: tmpUpload, originalname } = req.file;
+    const { path: tmpUpload, originalname} = req.file;
+    
     const fileName = `${_id}_${originalname}`;
+    
     const resultUpload = path.join(avatarsDir, fileName);
     await fs.rename(tmpUpload, resultUpload);
     const avatarUrl = path.join("avatar", fileName);
-    await User.findByIdAndUpdate(_id, { avatarUrl });
-
+     
+    await User.findByIdAndUpdate(_id, {
+        avatarUrl  
+    })
     res.json({
         avatarUrl,
     })
--- middlewares/index.js
+++ middlewares/index.js
@@ -3,6 +3,7 @@ const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
 const upload = require("./upload");
 
+
 module.exports = {
     validateBody,
     isValidId,
